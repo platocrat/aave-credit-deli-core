06d6d104e7d3cb6e3f7725ef2e20e489e98d037d
Jack Winfield <platocrat@tuta.io>
2021-01-25 16:36:24 -0600

    Completed storage contract

--- contracts/CreditDeliStorage.sol
+++ contracts/CreditDeliStorage.sol
@@ -4,15 +4,20 @@ pragma solidity 0.8.0;
 import {DelegationDataTypes} from "./DelegationDataTypes.sol";
 
 /**
- * @dev -------------------------- TODO ---------------------------------
- * This contract is analogous to Aave's `LendingPoolStorage`. Everything
+ * @dev This contract is analogous to Aave's `LendingPoolStorage`. Everything
  * other than core variable mappings and other storage variables must be
- * contained within the `DelegationLogic` contract.
- * ----------------------------------------------------------------------
+ * contained within `DelegationLogic.sol`.
  */
 contract CreditDeliStorage {
     using DelegationLogic for DelegationDataTypes.DelegationData;
 
     // Records the approved delegatees of each delegator.
-    mapping(address => DelegationData[]) internal Creditors;
+    mapping(address => DelegationDataTypes.DelegationData)
+        internal _delegations;
+    // List of available delegations, structured as a mapping for gas savings
+    // reasons
+    mapping(uint256 => address) internal _delegationsList;
+
+    // uint256 internal _delegationsCount;
+    // bool internal _paused;
 }
