927266c6e82cf62791c5be9adfb78e4af1edd3c3
Jack Winfield <platocrat@tuta.io>
2021-01-27 16:21:59 -0600

    Cleaned and added 3 new fields

--- contracts/DelegationLogic.sol
+++ contracts/DelegationLogic.sol
@@ -4,10 +4,9 @@ pragma solidity 0.8.0;
 import {DelegationDataTypes} from "./DelegationDataTypes.sol";
 
 /**
- * @dev -------------------------- TODO ---------------------------------
+ * @dev
  * This library is analogous to Aave's `ReserveLogic`. Implements the logic
  * to update a delegation's state.
- * ----------------------------------------------------------------------
  */
 library DelegationLogic {
     using DelegationLogic for DelegationDataTypes.DelegationData;
@@ -25,7 +24,10 @@ library DelegationLogic {
         address indexed delegator,
         address delegate, // address of borrower with an uncollateralized loan
         uint256 creditLine, // limit of total debt
-        uint256 debt // debt this borrower owes to the delegator
+        uint256 debt, // debt this borrower owes to the delegator
+        bool isApproved, // Does this delegation have an approved borrower?
+        bool hasFullyRepayed, // Has the borrower repayed their loan?
+        bool hasWithdrawn // Has the delegator withdrawn their deposit?
     );
 
     /**
@@ -41,22 +43,25 @@ library DelegationLogic {
         address indexed delegator,
         address delegate, // address of borrower with an uncollateralized loan
         uint256 creditLine, // limit of total debt
-        uint256 debt // debt this borrower owes to the delegator
+        uint256 debt, // **NEW** debt balance of the borrower that is now owed
+        bool isApproved, // Does this delegation have an approved borrower?
+        bool hasFullyRepayed, // Has the borrower repayed their loan?
+        bool hasWithdrawn // Has the delegator withdrawn their deposit?
     );
 
     /**
      * @dev Initializes a delegation by adding a borrower. Call this function
      *      **AFTER** a delegator has approved a borrower.
+     * @param _asset The address of the asset used in the delegation
      * @param _delegator The address of the creditor.
      * @param _delegate The address of borrower with an uncollateralized loan
      * @param _creditLine The bororwer's limit of total debt
-     * @param _asset The address of the asset used in the delegation
      */
     function init(
         _delegator,
+        _asset,
         _delegate,
-        _creditLine,
-        _asset
+        _creditLine
     ) external {
         // Initialize a delegation object.
         DelegationDataTypes.DelegationData storage delegation =
@@ -68,13 +73,93 @@ library DelegationLogic {
         delegation.creditLine = _creditLine;
         delegation.debt = 0;
         delegation.isApproved = true;
+        delegation.hasFullyRepayed = false;
+        delegation.hasWithdrawn = false;
 
         emit DelegationDataCreated(
             delegation.asset,
+            _delegator,
             delegation.delegate,
             delegation.creditLine,
             delegation.debt,
-            delegation.isApproved
+            delegation.isApproved,
+            delegation.hasFullyRepayed,
+            delegation.hasWithdrawn
+        );
+    }
+
+    /**
+     * @dev Add the debt amount for this delegation.
+     * @param _delegator The address of the creditor.
+     * @param _amountToBorrow The debt of the delegate is borrowing.
+     */
+    function addDebt(address _delegator, uint256 _amountToBorrow) external {
+        // Get the delegation for this delegator.
+        DelegationDataTypes.DelegationData storage delegation =
+            _delegations[_delegator];
+
+        // Set the borrower address to the `delegate` field of the object.
+        delegation.debt = _amountToBorrow;
+
+        emit DelegationDataUpdated(
+            delegation.asset,
+            _delegator,
+            delegation.delegate,
+            delegation.creditLine,
+            delegation.debt,
+            delegation.isApproved,
+            delegation.hasFullyRepayed,
+            delegation.hasWithdrawn
+        );
+    }
+
+    /**
+     * @dev Add whether the delegation has been repayed in full.
+     * @param _delegator The address of the creditor.
+     * @param _repayAmount The amount of debt to be repayed.
+     */
+    function addRepayment(address _delegator, uint256 _repayAmount) external {
+        // Get the delegation for this delegator.
+        DelegationDataTypes.DelegationData storage delegation =
+            _delegations[_delegator];
+
+        // Set debt owed to new balance.
+        delegation.debt = delegation.debt - _repayAmount;
+
+        if (delegation.hasFullyRepayed == 0) delegation.hasFullyRepayed == true;
+
+        emit DelegationDataUpdated(
+            delegation.asset,
+            _delegator,
+            delegation.delegate,
+            delegation.creditLine,
+            delegation.debt,
+            delegation.isApproved,
+            delegation.hasFullyRepayed,
+            delegation.hasWithdrawn
+        );
+    }
+
+    /**
+     * @dev Add whether the deposit for the delegation has been withdrawn.
+     * @param _delegator The address of the creditor.
+     */
+    function addWithdrawal(address _delegator) external {
+        // Get the delegation for this delegator.
+        DelegationDataTypes.DelegationData storage delegation =
+            _delegations[_delegator];
+
+        delegation.hasWithdrawn = true;
+
+        emit DelegationDataUpdated(
+            delegation.asset,
+            _delegator,
+            delegation.delegate,
+            delegation.creditLine,
+            delegation.debt,
+            delegation.isApproved,
+            delegation.hasFullyRepayed,
+            delegation.hasWithdrawn
         );
     }
 
