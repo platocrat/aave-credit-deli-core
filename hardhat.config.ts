f5bdf83afa241ecde1aada136aeb6cbdf746e218
Jack Winfield <platocrat@tuta.io>
2021-01-29 14:25:50 -0600

    Reverted so that I can still get DAI

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -34,7 +34,7 @@ export default {
          * in our `ts.config.json`, TypeScript config file!
          */
         url: process.env.FORKING_URL,
-        blockNumber: 11752611 // Fri, Jan 29
+        blockNumber: 11395144
       }
     }
   },
