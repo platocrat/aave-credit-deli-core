0b16b07549da77066fb33994987c05c0164e672a
Jack Winfield <platocrat@tuta.io>
2021-01-31 10:17:07 -0600

    Removed unnecessary code

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,36 +3,10 @@ import '@nomiclabs/hardhat-waffle'
 import '@nomiclabs/hardhat-ethers'
 import 'dotenv/config'
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task('accounts', 'Prints the list of accounts', async (
-  hre: {
-    ethers: {
-      getSigners: () => any
-    }
-  }
-) => {
-  const accounts = await hre.ethers.getSigners()
-
-  for (const account of accounts) {
-    console.log(account.address)
-  }
-})
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 export default {
   networks: {
     hardhat: {
       forking: {
-        /**
-         * @dev The line below now works thanks to `./env.d.ts` being included 
-         * in our `ts.config.json`, TypeScript config file!
-         */
         url: process.env.FORKING_URL,
         blockNumber: 11395144
       }
