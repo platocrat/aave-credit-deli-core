2ac23399960661f3e24c5c6927126d2eba50be44
Jack Winfield <platocrat@tuta.io>
2021-02-06 15:41:02 -0600

    Updated Kovan addresses

--- contracts/AaveCreditDelegationV2.sol
+++ contracts/AaveCreditDelegationV2.sol
@@ -53,9 +53,9 @@ contract AaveCreditDelegationV2 is CreditDeliStorage {
     address constant protocolDataProviderMainnetAddress =
         0x057835Ad21a177dbdd3090bB1CAE03EaCF78Fc6d;
     // address constant lendingPoolKovanAddress =
-    //     ;
+    //     0xE0fBa4Fc209b4948668006B2bE61711b7f465bAe;
     // address constant protocolDataProviderKovanAddress =
-    //     ;
+    //     0x3c73A5E5785cAC854D468F727c606C07488a29D6;
 
     ILendingPool constant lendingPool =
         ILendingPool(address(lendingPoolMainnetAddress));
